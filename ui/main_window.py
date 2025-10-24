e527ca4157c3746b0e8653541b4966c487165f3f
farza <farzanrichu610@gmail.com>
2025-10-25 02:52:03 +0530

    set up two buttons for start and stop

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,5 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow
-
+from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QPushButton, QHBoxLayout
 
 
 class MainWindow(QMainWindow):
@@ -10,3 +9,27 @@ class MainWindow(QMainWindow):
         self.show()
         self.initUI()
 
+
+
+    def initUI(self):
+        self.centralwidget = QWidget(self)
+        self.setCentralWidget(self.centralwidget)
+
+        self.main_layout = QVBoxLayout(self.centralwidget)
+
+        self.button_layout = QHBoxLayout()
+        self.start_button = QPushButton("Start")
+        self.stop_button = QPushButton("Stop")
+
+        self.start_button.clicked.connect(self.start_visualization)
+        self.stop_button.clicked.connect(self.stop_visualization)
+
+        for buttons in [self.start_button, self.stop_button]:
+            self.button_layout.addWidget(buttons)
+
+        self.main_layout.addLayout(self.button_layout)
+    def start_visualization(self):
+        pass
+    
+    def stop_visualization(self):
+        pass
