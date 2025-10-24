e1e3f7795243ddc0353edf428eb5e99833b4edda
farza <farzanrichu610@gmail.com>
2025-10-25 03:22:02 +0530

    added the grpah layout here and a little bit mistake correction

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QPushButton, QHBoxLayout
-
+from visualizer.waveform_plot import WaveformPlot
+from visualizer.spectrum_plot import SpectrumPlot
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -9,14 +10,21 @@ class MainWindow(QMainWindow):
         self.show()
         self.initUI()
 
-
-
     def initUI(self):
         self.centralwidget = QWidget(self)
         self.setCentralWidget(self.centralwidget)
 
         self.main_layout = QVBoxLayout(self.centralwidget)
 
+        #visualizer layout
+        self.visualizer_layout = QHBoxLayout()
+        self.time_domain_graph=WaveformPlot()
+        self.freq_domain_graph=SpectrumPlot()
+
+        for plots in [self.time_domain_graph, self.freq_domain_graph]:
+            self.visualizer_layout.addWidget(plots)
+
+        # button layout
         self.button_layout = QHBoxLayout()
         self.start_button = QPushButton("Start")
         self.stop_button = QPushButton("Stop")
@@ -27,9 +35,11 @@ class MainWindow(QMainWindow):
         for buttons in [self.start_button, self.stop_button]:
             self.button_layout.addWidget(buttons)
 
-        self.main_layout.addLayout(self.button_layout)
+        for layout in [self.visualizer_layout, self.button_layout]:
+            self.main_layout.addWidget(layout)
+
     def start_visualization(self):
         pass
-    
+
     def stop_visualization(self):
         pass
