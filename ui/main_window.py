eba08ee58b4d87e6ae019b5218a7738eaba11234
farza <farzanrichu610@gmail.com>
2025-10-25 18:10:33 +0530

    integration done

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,14 +1,19 @@
 from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QPushButton, QHBoxLayout
 from visualizer.waveform_plot import WaveformPlot
 from visualizer.spectrum_plot import SpectrumPlot
+from audio.audio_stream import AudioStream
+from audio.audio_process import compute_fft
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Audio Visualizer")
         self.resize(500, 500)
-        self.show()
         self.initUI()
+        self.show()
+        self.stream_started=False
+        self.audio_stream = AudioStream(callback=self.update_plots)
+
 
     def initUI(self):
         self.centralwidget = QWidget(self)
@@ -16,7 +21,7 @@ class MainWindow(QMainWindow):
 
         self.main_layout = QVBoxLayout(self.centralwidget)
 
-        #visualizer layout
+            #visualizer layout
         self.visualizer_layout = QHBoxLayout()
         self.time_domain_graph=WaveformPlot()
         self.freq_domain_graph=SpectrumPlot()
@@ -24,7 +29,7 @@ class MainWindow(QMainWindow):
         for plots in [self.time_domain_graph, self.freq_domain_graph]:
             self.visualizer_layout.addWidget(plots)
 
-        # button layout
+            # button layout
         self.button_layout = QHBoxLayout()
         self.start_button = QPushButton("Start")
         self.stop_button = QPushButton("Stop")
@@ -39,7 +44,19 @@ class MainWindow(QMainWindow):
             self.main_layout.addLayout(layout)
 
     def start_visualization(self):
-        pass
-
+        if not self.stream_started:
+            self.audio_stream.start()
+            self.stream_started=True
     def stop_visualization(self):
-        pass
+        self.audio_stream.stop()
+        self.stream_started=False
+
+
+
+    def update_plots(self,data):
+        self.time_domain_graph.update_plot(data)
+
+        fft_data, fft_freqs = compute_fft(data, self.audio_stream.sample_rate)
+
+        self.freq_domain_graph.update_plot(fft_data,fft_freqs)
+
