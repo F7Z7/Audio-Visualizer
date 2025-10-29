6b716a9b80c9d37c1a3b719c72e57021fe82ef31
farza <farzanrichu610@gmail.com>
2025-10-30 00:22:34 +0530

    logic

--- audio/audio_file_input.py
+++ audio/audio_file_input.py
@@ -1,30 +1,41 @@
 import soundfile as sf
-import numpy as np
+import sounddevice as sd
 import time
+import numpy as np
 
 class AudioFileInput:
-    def __init__(self, audio_file_path,chunk_size=1024, callback=None):
+    def __init__(self, audio_file_path, chunk_size=1024, callback=None):
         self.audio_file_path = audio_file_path
         self.chunk_size = chunk_size
         self.callback = callback
-        self.stream_running=False
-
+        self.stream_running = False
 
     def start(self):
-        data,samplerate = sf.read(self.audio_file_path,dtype='float32')
+        data, samplerate = sf.read(self.audio_file_path, dtype='float32')
         self.samplerate = samplerate
-        self.stream_running=True
+        self.stream_running = True
+
+        num_chunks = len(data) // self.chunk_size
 
 
-        num_chunks=len(self.data)//self.chunk_size
+        with sd.OutputStream(samplerate=self.samplerate, channels=data.shape[1] if data.ndim > 1 else 1):
+            for i in range(num_chunks):
+                if not self.stream_running:
+                    break
 
-        for i in range(num_chunks):
-            if not self.stream_running:
-                break
-            chunk = data[i * self.chunk_size: (i + 1) * self.chunk_size]
-            if self.callback:
-                self.callback(chunk)
-            time.sleep(self.chunk_size / self.sample_rate)
+                chunk = data[i * self.chunk_size: (i + 1) * self.chunk_size]
+
+                sd.wait()
+                sd.play(chunk, self.samplerate, blocking=False)
+
+                if self.callback:
+                    if data.ndim > 1:
+                        self.callback(np.mean(chunk, axis=1))
+                    else:
+                        self.callback(chunk)
+
+                time.sleep(self.chunk_size / self.samplerate)
 
     def stop(self):
-        self.stream_running=False
+        self.stream_running = False
+        sd.stop()
