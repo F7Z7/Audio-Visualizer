1636adc4db8b62311818c9cc82854bd53ff707af
farza <farzanrichu610@gmail.com>
2025-11-07 23:18:55 +0530

    made the audio input channel from microphone to device stereo input

--- audio/audio_stream.py
+++ audio/audio_stream.py
@@ -2,22 +2,38 @@ import sounddevice as sd
 
 
 class AudioStream:
-    def __init__(self,sample_rate=44100,  chunk_size=1024, callback=None):
+    def __init__(self,sample_rate=44100,  chunk_size=1024, callback=None,use_system_audio=True):
         self.sample_rate = sample_rate
         self.chunk_size = chunk_size
         self.callback = callback
         self.stream=None
+        self.use_system_audio=use_system_audio
 
     def start(self):
-        self.stream=sd.InputStream(channels=1, samplerate=self.sample_rate,
-                       blocksize=self.chunk_size,
-                       callback=self._audio_callback)
+
+        device=None
+        if self.use_system_audio:
+            devices=sd.query_devices()
+            for i, d in enumerate(devices):
+                if 'loopback' in d['name'].lower() or 'stereo mix' in d['name'].lower():
+                    device = i
+                    print(f"[INFO] Using loopback device: {d['name']}")
+                    break
+
+        self.stream=sd.InputStream(
+            device=device,
+            channels=2,
+            samplerate=self.sample_rate,
+            blocksize=self.chunk_size,
+            callback=self._audio_callback)
         self.stream.start()
 
 
     def _audio_callback(self, indata, frames, time, status):
         if self.callback:
             self.callback(indata[:, 0])
+            mono = indata.mean(axis=1)
+            self.callback(mono)
 
     def stop(self):
         if self.stream:
