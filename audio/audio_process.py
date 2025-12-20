d3e3a5ed62295853f69cfb31be31bdfe0ac88269
Farzan R S <farzanrichu610@gmail.com>
2025-12-21 00:54:04 +0530

    mag smooth

--- audio/audio_process.py
+++ audio/audio_process.py
@@ -7,7 +7,7 @@ def compute_fft(audio_chunk, sample_rate):
     windowed = audio_chunk * window
 
     fft_vals = np.fft.rfft(windowed)
-    magnitude = np.abs(fft_vals)
+    magnitude = np.abs(fft_vals) / (n / 2)
     freqs = np.fft.rfftfreq(n, 1 / sample_rate)
 
     return magnitude, freqs
